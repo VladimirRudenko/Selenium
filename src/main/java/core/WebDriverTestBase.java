ab5d5917483ab599ede039b0410e4252e367598a
vladimir1rudenko@gmail.com <vladimir1rudenko@gmail.com>
2017-03-27 14:59:40 +0300

    commit

--- src/main/java/core/WebDriverTestBase.java
+++ src/main/java/core/WebDriverTestBase.java
@@ -1,7 +1,30 @@
 package core;
 
+;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+
 /**
  * Created by vladimir on 23.03.17.
  */
-public class WebDriverTestBase {
+public abstract class WebDriverTestBase {
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver", "/home/vladimir/Selenium/src/test/resources/geckodriver");
+         driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+    @AfterClass
+    public void tearDown(){driver.quit();}
+
+
 }
